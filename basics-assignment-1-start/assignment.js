4ce4315e7b2b8f18b8b569a4636d82e522fcfb18
Salifu Zeba <salifu.zeba@amalitech.org>
2020-11-03 18:05:56 +0000

    assignment1

---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,13 @@
+let userInput = 30;
+let result;
+result = 18+userInput;
+
+result = result - 25;
+result = result/2
+result = result*5;
+
+
+//sthe userInput value will never change
+
+alert(userInput);
+alert(result)
