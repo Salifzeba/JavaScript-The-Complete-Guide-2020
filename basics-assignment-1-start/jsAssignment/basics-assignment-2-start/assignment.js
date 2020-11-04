d56dd0926c23410e8bb2fd6a14de5bed66d47b26
Salifu Zeba <salifu.zeba@amalitech.org>
2020-11-04 11:05:58 +0000

    assignmeent1
    variables

---
+++ basics-assignment-1-start/jsAssignment/basics-assignment-2-start/assignment.js
@@ -0,0 +1,17 @@
+function hello(){
+    alert("hello world")
+    
+}
+hello()
+function salifu(hello) {alert(hello)}
+
+const btn = document.getElementById("task-3");
+btn.addEventListener('click',  hello)
+function zeba(gordon,sharaf,kristine){
+   return gordon +" "+sharaf +""+kristine
+}
+
+
+console.log(zeba("gordon","sharaf", "kristine"))                        
+
+
